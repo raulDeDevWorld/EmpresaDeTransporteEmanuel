d1840876c058ae2803fae231d7f9f61ed173e831
raulDeDevWorld <proyection009@gmail.com>
2023-01-27 02:10:29 -0400

    DC

--- firebase/config.js
+++ firebase/config.js
@@ -1,11 +1,21 @@
-export const firebaseConfig = {
-
-  apiKey: "AIzaSyA7xgVg-cr8LLlB03ZM-LxxIoInz67W2MQ",
-  authDomain: "qr-data-5774c.firebaseapp.com",
-  databaseURL: "https://qr-data-5774c-default-rtdb.firebaseio.com",
-  projectId: "qr-data-5774c",
-  storageBucket: "qr-data-5774c.appspot.com",
-  messagingSenderId: "635643222593",
-  appId: "1:635643222593:web:5972759e776e780029c4c0"
-  
+export  const firebaseConfig = {
+    apiKey: "AIzaSyA6XEgYhw5Y0tlHf96xwBfP4zqGd-3vr_M",
+    authDomain: "qr-data-v2.firebaseapp.com",
+    databaseURL: "https://qr-data-v2-default-rtdb.firebaseio.com",
+    projectId: "qr-data-v2",
+    storageBucket: "qr-data-v2.appspot.com",
+    messagingSenderId: "74682152920",
+    appId: "1:74682152920:web:53718f1dd762e46a9424ad"
   };
+
+// export const firebaseConfig = {
+
+//   apiKey: "AIzaSyA7xgVg-cr8LLlB03ZM-LxxIoInz67W2MQ",
+//   authDomain: "qr-data-5774c.firebaseapp.com",
+//   databaseURL: "https://qr-data-5774c-default-rtdb.firebaseio.com",
+//   projectId: "qr-data-5774c",
+//   storageBucket: "qr-data-5774c.appspot.com",
+//   messagingSenderId: "635643222593",
+//   appId: "1:635643222593:web:5972759e776e780029c4c0"
+  
+//   };
